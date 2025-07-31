b7ee97ba76078552e46feaf9e187446685b097aa
gk <promise_ghostking@163.com>
2025-07-31 22:53:48 +0800

    feat: 添加图片预加载插件

--- play/vite.config.ts
+++ play/vite.config.ts
@@ -6,9 +6,11 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), imageFallback({
-    defaultImgUrl: 'src/assets/image10.jpg',
-    // defaultImgUrl: 'https://img2.baidu.com/it/u=3018303209,1765139986&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=722',
-  }), imagesPrefetch()],
+    // defaultImgUrl: 'src/assets/image10.jpg',
+    defaultImgUrl: 'https://img2.baidu.com/it/u=3018303209,1765139986&fm=253&fmt=auto&app=120&f=JPEG?w=500&h=722',
+  }), imagesPrefetch({
+    dir: 'src/assets',
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
--- vite-plugins/src/imagesPrefetch/index.ts
+++ vite-plugins/src/imagesPrefetch/index.ts
@@ -1,7 +1,47 @@
 import type { Plugin } from 'vite'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
 
-export function imagesPrefetch(): Plugin {
+export interface Options {
+  /**
+   *  需要预加载的图片文件夹
+   */
+  dir: string
+  rel?: 'preload' | 'prefetch'
+}
+export function imagesPrefetch(options: Options): Plugin {
+  const { dir, rel = 'preload' } = options
+  const assetsImages: string[] = []
+
+  const images = fs.readdirSync(dir)
+    .filter(file => /\.(?:jpg|jpeg|png|gif|svg)$/.test(file))
+    .map(file => path.join(dir, file).replaceAll('\\', '/'))
+
   return {
-    name: 'vite-plugin-images-prefetch',
+    name: 'vite-plugin-prefetch-images',
+    transformIndexHtml(_, ctx) {
+      let _images: string[]
+      ctx.server ? (_images = images) : (_images = assetsImages)
+
+      return _images.map((file) => {
+        return {
+          tag: 'link',
+          attrs: {
+            as: 'image',
+            rel,
+            href: file,
+          },
+        }
+      })
+    },
+    generateBundle(_, bundle) {
+      const values = Object.values(bundle)
+
+      values.forEach((item) => {
+        if (images.includes(Reflect.get(item, 'originalFileName'))) {
+          assetsImages.push(item.fileName)
+        }
+      })
+    },
   }
 }
