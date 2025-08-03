9edb0a256177c9227ed90f8933326c908c5bb45b
gk <promise_ghostking@163.com>
2025-08-03 16:09:46 +0800

    chore: watch 添加 immediate 和 once 属性

--- vue/packages/reactivity/src/watch.ts
+++ vue/packages/reactivity/src/watch.ts
@@ -5,19 +5,43 @@ import { isRef } from './ref'
 type WatchCallback = (newValue: any, oldValue: any) => void
 type WatchSource = RefImpl
 type WatchStopHandle = () => void
+interface WatchOptions {
+  immediate?: boolean
+  once?: boolean
+  deep?: boolean | number
+}
 
-export function watch(source: WatchSource, watchCallback: WatchCallback): WatchStopHandle {
+export function watch(source: WatchSource, watchCallback: WatchCallback, options?: WatchOptions): WatchStopHandle {
+  const { immediate, once } = options || {}
+
+  if (once) {
+    const _watchCallback = watchCallback
+    watchCallback = (...args) => {
+      _watchCallback(...args)
+      stop()
+    }
+  }
+
   let getter!: () => any
 
   if (isRef(source)) {
     getter = () => source.value
   }
 
+  let oldValue: undefined
+
   const effect = new ReactiveEffect(getter)
 
-  let oldValue = effect.run()
+  if (immediate) {
+    job()
+  }
+  else {
+    oldValue = effect.run()
+  }
 
-  effect.scheduler = () => {
+  effect.scheduler = job
+
+  function job() {
     const newValue = effect.run()
     watchCallback(newValue, oldValue)
 
