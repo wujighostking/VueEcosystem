cf67b79aa27dad6e5bdf8115b823d1062e62ba06
gk <promise_ghostking@163.com>
2025-09-25 00:35:46 +0800

    feat: 基本实现 history 路由模式

--- vue-router/src/index.ts
+++ vue-router/src/index.ts
@@ -1,9 +1,52 @@
 type PathType = string | URL | null
-type State = ReturnType<typeof buildState>
+type HistoryStateNavigation = ReturnType<typeof buildState>
 
 export function createWebHistory() {
-  // eslint-disable-next-line unused-imports/no-unused-vars
   const historyNavigation = useHistoryStateNavigation()
+
+  const historyListeners = useHistoryListeners(historyNavigation.state, historyNavigation.location)
+
+  const routerHistory = Object.assign({}, historyNavigation, historyListeners)
+
+  Object.defineProperties(routerHistory, {
+    location: {
+      get: () => historyNavigation.location.value,
+    },
+    state: {
+      get: () => historyNavigation.state.value,
+    },
+  })
+
+  return routerHistory
+}
+
+function useHistoryListeners(historyState: ReturnType<typeof useHistoryStateNavigation>['state'], currentLocation: ReturnType<typeof useHistoryStateNavigation>['location']) {
+  const listeners: ((to: PathType, from: PathType, options: { isBack: boolean }) => void)[] = []
+  const popStatehandler = ({ state }: PopStateEvent) => {
+    const to = createCurrentLocation()
+    const from = currentLocation.value
+    const fromState = historyState.value
+
+    currentLocation.value = to
+    historyState.value = state
+
+    // 小于 0 表示回退
+    const isBack = state.position - fromState.position < 0
+
+    listeners.forEach((listener) => {
+      listener(to, from, { isBack })
+    })
+  }
+
+  window.addEventListener('popstate', popStatehandler)
+
+  function listen(callback: (to: PathType, from: PathType, options: { isBack: boolean }) => void) {
+    listeners.push(callback)
+  }
+
+  return {
+    listen,
+  }
 }
 
 /**
@@ -23,13 +66,13 @@ function useHistoryStateNavigation() {
     changeLocation(currentLocation.value, buildState(null, currentLocation.value, null, true), true)
   }
 
-  function changeLocation(to: PathType, state: any, repalce: boolean) {
-    window.history[repalce ? 'replaceState' : 'pushState'](state, '', to)
+  function changeLocation(to: PathType, state: any, replace: boolean) {
+    window.history[replace ? 'replaceState' : 'pushState'](state, '', to)
     historyState.value = state
   }
 
   function push(to: PathType, data: Record<any, any>) {
-    const curretnState: State = Object.assign(
+    const curretnState: HistoryStateNavigation = Object.assign(
       {},
       historyState.value,
       { forward: to, scroll: { left: window.pageXOffset, top: window.pageYOffset } },
@@ -46,7 +89,7 @@ function useHistoryStateNavigation() {
     currentLocation.value = to
   }
 
-  function repalce(to: PathType, data: Record<any, any>) {
+  function replace(to: PathType, data: Record<any, any>) {
     const state = Object.assign({}, buildState(historyState.value.back, to, historyState.value.forward, true), data)
 
     changeLocation(to, state, true)
@@ -59,7 +102,7 @@ function useHistoryStateNavigation() {
     location: currentLocation,
     state: historyState,
     push,
-    repalce,
+    replace,
   }
 }
 
@@ -69,12 +112,12 @@ function createCurrentLocation(): PathType {
   return pathname + search + hash
 }
 
-function buildState(back: PathType, current: PathType, forward: PathType, repalce: boolean = false, computedScroll: boolean = false) {
+function buildState(back: PathType, current: PathType, forward: PathType, replace: boolean = false, computedScroll: boolean = false) {
   return {
     back,
     current,
     forward,
-    replace: repalce,
+    replace,
     scroll: computedScroll ? { left: window.pageXOffset, top: window.pageYOffset } : null,
     position: window.history.length - 1,
   }
