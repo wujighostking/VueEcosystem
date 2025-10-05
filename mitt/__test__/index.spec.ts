706447b695301e306dac1c79c6c338a43199fd4c
gk <promise_ghostking@163.com>
2025-10-05 21:02:31 +0800

    test: 添加 mitt 测试代码

---
+++ mitt/__test__/index.spec.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it, vi } from 'vitest'
+import { mitt, useMitt } from '../src'
+
+describe('test mitt', () => {
+  const emitter = useMitt()
+
+  it('test mitt is single instance', () => {
+    expect(emitter).toBe(mitt())
+  })
+
+  it('test mitt', () => {
+    const onEvent = vi.fn(() => {})
+
+    emitter.on('onEvent', onEvent)
+    emitter.emit('onEvent')
+
+    expect(onEvent).toBeCalledTimes(1)
+
+    emitter.off('onEvent', onEvent)
+    emitter.emit('onEvent')
+
+    expect(onEvent).toBeCalledTimes(1)
+  })
+
+  it('once', () => {
+    const onEvent = vi.fn(() => {})
+
+    emitter.once('onEvent', onEvent)
+    emitter.emit('onEvent')
+
+    expect(onEvent).toBeCalledTimes(1)
+  })
+
+  it('test clear', () => {
+    const onEvent1 = vi.fn(() => {})
+    const onEvent2 = vi.fn(() => {})
+
+    emitter.on('onEvent1', onEvent1)
+    emitter.on('onEvent2', onEvent2)
+
+    emitter.all.clear()
+
+    expect(onEvent1).toBeCalledTimes(0)
+    expect(onEvent2).toBeCalledTimes(0)
+  })
+})
